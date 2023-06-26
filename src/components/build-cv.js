2b192edf28e2a4e5e03794e8cfb064c8fb12acda
Jeremiah Carey <jeremiah.a.carey@gmail.com>
2023-06-26 11:18:47 -0400

    cv-display component done

--- src/components/build-cv.js
+++ src/components/build-cv.js
@@ -6,15 +6,14 @@ import {
 
 export class BuildCV extends LitElement {
   static styles = css`
-    @import url("https://fonts.googleapis.com/css2?family=Mulish&display=swap");
-
     :host {
       font-family: "Mulish", sans-serif;
+      font-size: 1em
       box-sizing: border-box;
     }
 
     .cv-container {
-      padding: 2em;
+      padding: 2rem;
       display: flex;
       justify-content: space-between;
       max-width: 900px;
@@ -24,28 +23,30 @@ export class BuildCV extends LitElement {
     .left-column {
       width: 75%;
       border-right: 2px solid black;
+      height: 100%;
     }
 
     .basic-info {
       border-bottom: 2px solid black;
-      padding: 1.25em;
+      padding: 1.25rem;
     }
 
     .right-column {
       width: 25%;
-      padding: 1.25em;
+      padding: 1.25rem;
       display: flex;
       flex-direction: column;
-      gap: 1em;
+      gap: 1rem;
     }
 
     .main-cat {
       font-weight: bold;
-      font-size: 1em;
+      font-size: 1rem;
       text-transform: uppercase;
       border: 2px solid black;
-      padding: 0.25em;
+      padding: 0.25rem;
       width: fit-content;
+      margin-top: 0;
     }
 
     .main-cat.right {
@@ -53,8 +54,8 @@ export class BuildCV extends LitElement {
     }
 
     .hello {
-      font-weight: bold;
-      font-size: 4em;
+      font-weight: 800;
+      font-size: 3.75rem;
       margin: 0;
     }
 
@@ -64,7 +65,7 @@ export class BuildCV extends LitElement {
     }
 
     .experience {
-      padding: 1.25em;
+      padding: 1.25rem;
     }
 
     .experience-group {
@@ -75,40 +76,41 @@ export class BuildCV extends LitElement {
       display: flex;
       flex-direction: column;
       justify-content: space-between;
-      gap: 2em;
+      gap: 2rem;
     }
 
     .experience-title {
-      font-size: 2em;
+      font-size: 2rem;
       border: none;
       padding-left: 0;
     }
 
     .job-dates {
       font-weight: bold;
-      font-size: 0.9rem;
+      font-size: 0.7rem;
       text-transform: uppercase;
       transform: translateX(-50%) rotate(-90deg);
       position: absolute;
       text-align: center;
       top: 45%;
-      left: 0.9em;
+      left: 0.9rem;
       letter-spacing: 0.1rem;
     }
 
     .job-role {
       font-weight: bold;
       text-transform: uppercase;
-      margin-left: 3em;
+      margin-left: 3rem;
     }
 
     .job-company {
       font-style: italic;
-      margin-left: 3em;
+      margin-left: 3rem;
     }
 
     .job-description {
-      margin-left: 3em;
+      margin-left: 3rem;
+      font-size: 0.87rem;
     }
 
     .education-group {
@@ -123,11 +125,11 @@ export class BuildCV extends LitElement {
     .skills-list {
       list-style: none;
       margin: 0;
-      padding: 1em 0 0 0;
+      padding: 1rem 0 0 0;
     }
 
     .skill {
-      line-height: 1.6em;
+      line-height: 1.6rem;
     }
   `;
 
@@ -136,7 +138,6 @@ export class BuildCV extends LitElement {
     name: { type: String },
     summary: { type: String },
     experience: { type: Array },
-    projects: { type: Array },
     contact: { type: Object },
     education: { type: Array },
     skills: { type: Array },
@@ -145,7 +146,7 @@ export class BuildCV extends LitElement {
   constructor() {
     super();
     this.role = "Software Engineer";
-    this.name = "Fitzwilliam Darcy";
+    this.name = "Ichabod Crane";
     this.summary =
       "Frontend engineer experienced in developing modular full-stack JavaScript applications using WebComponents, React, Node, Express, RESTful APIs, and SQL databases.";
     this.experience = [
@@ -157,41 +158,36 @@ export class BuildCV extends LitElement {
           "Lorem ipsum dolor sit amet. At sunt tempora ea labore recusandae ab rerum mollitia non voluptatibus eligendi id quos temporibus et obcaecati dolorem. Sit odio aspernatur At amet incidunt At ullam optio nam dicta quia.",
       },
       {
-        dates: "2021-2022",
+        dates: "2021",
+        role: "Junior Web Developer",
+        company: "Bob's Builders Online",
+        description:
+          "A sint labore 33 quos galisum a reiciendis excepturi aut corrupti quod aut nostrum porro. Est adipisci nihil et sapiente perferendis ut laborum commodi et voluptates enim vel reiciendis aliquam qui sequi corrupti sit quod voluptas. Non eaque pariatur est ullam nihil id exercitationem delectus est suscipit galisum eos aliquam harum.",
+      },
+      {
+        dates: "2020-2021",
         role: "Intern, Web Development",
         company: "Bob's Builders Online",
         description:
           "A sint labore 33 quos galisum a reiciendis excepturi aut corrupti quod aut nostrum porro. Est adipisci nihil et sapiente perferendis ut laborum commodi et voluptates enim vel reiciendis aliquam qui sequi corrupti sit quod voluptas. Non eaque pariatur est ullam nihil id exercitationem delectus est suscipit galisum eos aliquam harum.",
       },
     ];
-    this.projects = [
-      {
-        title: "Movie Review App",
-        description:
-          "Aut obcaecati dolor ut dolore error qui quod excepturi ut veritatis Quis sit dolores nisi et optio dolorum. Eos tempora officia in eius numquam et dolores aspernatur aut nobis voluptatem ut fuga similique aut libero possimus ut obcaecati atque.",
-      },
-      {
-        title: "To Do List App",
-        description:
-          "Qui officiis eligendi ut architecto officia et molestiae velit nam consequatur consequuntur aut consectetur eveniet aut aliquid esse eum amet nesciunt? At praesentium maxime sed nemo consequuntur non libero dolores eos galisum voluptas nam accusantium debitis. Et animi autem aut ipsum fugit qui sint nostrum At illo dolorem in iste provident aut iusto dolores et nostrum ducimus. Qui distinctio natus rem iste soluta At animi illum.",
-      },
-    ];
     this.contact = {
-      email: "abobhenry@webdev.com",
+      email: "ichabod@crane.dev",
       phone: "123-456-7890",
-      site: "www.abobhenry.dev",
+      site: "www.crane.dev",
       city: "Orlando, FL",
     };
     this.education = [
       {
         dateFinished: "2017",
         degree: "Ph.D. Philosophy",
-        school: "U.C. Berkeley",
+        school: "UCLA",
       },
       {
         dateFinished: "2007",
         degree: "B.A. Philosophy",
-        school: "The Florida State University",
+        school: "University of Guam",
       },
     ];
     this.skills = [
@@ -208,12 +204,18 @@ export class BuildCV extends LitElement {
 
   render() {
     return html`
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700;800&display=swap"
+        rel="stylesheet"
+      />
       <div class="cv-container">
         <div class="left-column">
           <div class="basic-info">
             <h1 class="main-cat">${this.role}</h1>
             <p class="hello">Hello, I'm</p>
-            <p class="hello">${this.name}</p>
+            <p class="hello hello-name">${this.name}</p>
             <p class="summary">${this.summary}</p>
           </div>
           <div class="experience">

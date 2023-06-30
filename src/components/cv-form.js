eff62d928089312fd2c4eb207a94f6dea1a6ab6d
Jeremiah Carey <jeremiah.a.carey@gmail.com>
2023-06-30 17:43:56 -0400

    Add input group component, start to make use in cv-form component

--- src/components/cv-form.js
+++ src/components/cv-form.js
@@ -1,5 +1,6 @@
 import { LitElement, css, html } from "lit";
 import { CollapsibleDiv } from "./collapsible-div";
+import { InputGroup } from "./input-group";
 
 export class CVForm extends LitElement {
   static properties = {
@@ -31,19 +32,44 @@ export class CVForm extends LitElement {
     return html`
       <div class="form-container">
         <collapsible-div top-label="Title/Role">
-          <div class="input-group">
-            <label>Job Title</label>
-            <input type="text" placeholder=${this.resData.role} />
-          </div> </collapsible-div
+          <input-group
+            form-label="Job Title"
+            input-type="text"
+            placeholder-info="${this.resData.role}"
+          ></input-group> </collapsible-div
         ><collapsible-div top-label="Name">
-          <div class="input-group">
-            <label>Name</label>
-            <input type="text" placeholder=${this.resData.name} />
-          </div>
+          <input-group
+            form-label="Name"
+            input-type="text"
+            placeholder-info="${this.resData.name}"
+          ></input-group>
         </collapsible-div>
         <collapsible-div top-label="Personal Summary">
+          <input-group
+            form-label="Summary"
+            input-type="textarea"
+            placeholder-info="${this.resData.summary}"
+          ></input-group>
+        </collapsible-div>
+        <collapsible-div top-label="Experience">
+          <div class="input-group">
+            <label>Role</label>
+            <input type="text" placeholder=${this.resData.experience.role} />
+          </div>
+          <div class="input-group">
+            <label>Company</label>
+            <input type="text" placeholder=${this.resData.experience.company} />
+          </div>
+          <div class="input-group">
+            <label>Dates Worked</label>
+            <input type="text" placeholder=${this.resData.experience.dates} />
+          </div>
           <div class="input-group">
             <label>Summary</label>
+            <textarea>${this.resData.summary} </textarea>
+          </div>
+          <div class="input-group">
+            <label>Description of Work Done</label>
             <textarea>${this.resData.summary} </textarea>
           </div>
         </collapsible-div>
